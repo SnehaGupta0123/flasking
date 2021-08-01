d6ac74eebad391577b2479cc188cd151d8b8520a
Sneha Gupta <snehaguptaprg@gmail.com>
2021-08-01 20:53:31 +0530

    some changes.......

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # FILTERS!!!
 #safe
@@ -12,6 +16,12 @@ from flask import Flask, render_template
 
 # Create Flask Instance 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "mysupersecretkey"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route('/')
@@ -39,3 +49,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!!")
+    return render_template('name.html', name=name, form=form)
